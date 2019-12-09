9587510d5c91f5987c3826ea16ea1893c2404fe3
Gweezdo <z.trivium@gmail.com>
2019-12-09 13:43:07 +1100

    Final Solution Code passed all the FCC tests Code properly commented

--- Project2.js
+++ Project2.js
@@ -1,38 +1,15 @@
 function convertToRoman(num) {
 
-    // if(num>1){
-    //     let ones = num%10;
-    //     num = Math.floor(num/10)
-    //     console.log("ones = " + ones);
-    //     console.log(num);
-
-    //     if(num > 1){ 
-    //         let tens = num%10*10;
-    //         num = Math.floor(num/10);
-    //         console.log("tens = " + tens);
-    //         console.log(num);
-    //     }
-        
-    // }
-
-    
-
-    // if(num > 100){
-    //     let hundreds = num%1000 *100;
-    //     console.log(hundreds);
-    // }
-
-    let strInt = num.toString();
+    let strInt = num.toString(); // Convert num to String to enable spliting into array
     let numArray = strInt.split("")
-    console.log(numArray);
 
-    let romanNumArr = [
-        {
+    let romanNumArr = [         // Define a object that holds all the Decimal : Roman Numeral key value pairs
+        {                       
             1: "I",
             2: "II",
             3: "III",
             4: "IV",
-            5: "V",
+            5: "V",             // First object holds numbers 1-9
             6: "VI",
             7: "VII",
             8: "VIII",
@@ -43,7 +20,7 @@ function convertToRoman(num) {
             2: "XX",
             3: "XXX",
             4: "XL",
-            5: "L",
+            5: "L",             // Second object holds numbers 10-90
             6: "LX",
             7: "LXX",
             8: "LXXX",
@@ -54,7 +31,7 @@ function convertToRoman(num) {
             2: "CC",
             3: "CCC",
             4: "CD",
-            5: "D",
+            5: "D",             // Third object holds numbers 100-900
             6: "DC",
             7: "DCC",
             8: "DCCC",
@@ -65,7 +42,7 @@ function convertToRoman(num) {
             2: "MM",
             3: "MMM",
             4: "MV\u0305",
-            5: "V\u0305",
+            5: "V\u0305",      // Forth object holds numbers 1000-9000     
             6: "V\u0305M",
             7: "V\u0305MM",
             8: "V\u0305MMM",
@@ -76,7 +53,7 @@ function convertToRoman(num) {
             2: "X\u0305X\u0305",
             3: "X\u0305X\u0305X\u0305",
             4: "L\u0305X\u0305",
-            5: "L\u0305",
+            5: "L\u0305",                   // Fith object holds numbers 10,000-90,000
             6: "X\u0305L\u0305",
             7: "X\u0305L\u0305L\u0305",
             8: "X\u0305L\u0305L\u0305L\u0305",
@@ -87,7 +64,7 @@ function convertToRoman(num) {
             2: "C\u0305C\u0305",
             3: "C\u0305C\u0305C\u0305",
             4: "C\u0305D\u0305",
-            5: "D\u0305",
+            5: "D\u0305",               // Sixth object holds numbers 100,000-900,000
             6: "C\u0305D\u0305",
             7: "C\u0305D\u0305D\u0305",
             8: "C\u0305D\u0305D\u0305D\u0305",
@@ -95,22 +72,23 @@ function convertToRoman(num) {
         },   
         {
             1: "M\u0305",
-            2: "M\u0305M\u0305",
-            3: "M\u0305M\u0305M\u0305",
-        }
+            2: "M\u0305M\u0305",        // Seventh object holds numbers 1,000,000-3,000,000 (The roman numeral for 5 Million)
+            3: "M\u0305M\u0305M\u0305", // is outside of the scope of this project, plus can't find this value on internet.
+        }                               // This symbol will have an obvious effect on 4 Million and why its not included)
     ]
 
-    
-    let reversedArr = numArray.reverse();
-
-    let arrLength = reversedArr.length -1;
-    console.log("reversedArr = " + reversedArr);
-    
-    for(var i=0; i=arrLength; i++){
-        console.log(romanNumArr[i][numArray[i]])
+    let reversedArr = numArray.reverse(); // reverse array in place so I can match the "one's value" with that of the Object  
+                                          // in the for loop to follow. i.e. i=0   
+    let finalRomanNumArr = [];
+    for(let i=0; i<reversedArr.length; i++){
+        console.log(romanNumArr[i][reversedArr[i]])             // iterate through number array and push roman numerals to
+        finalRomanNumArr.push(romanNumArr[i][reversedArr[i]])   //empty array
     }
 
- return num;
+    let finalNum = finalRomanNumArr.reverse().join("");         // the array is currently in the order 1, 10, 100 ..etc, so
+    console.log(finalNum);                                      // reverse in place and join() to form a string 
+
+ return finalNum;
 }
 
 
